098ae1cc5d9c05439138799a4eb3ee129190646e
ankitabaler <balerankita@gmail.com>
2026-09-25 09:29:11 +0530

    Add files via upload

---
+++ SparseArrayssolution.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY stringList
+ *  2. STRING_ARRAY queries
+ */
+
+vector<int> matchingStrings(vector<string> stringList, vector<string> queries) {
+    unordered_map<string, int> frequencyMap;
+    
+    // Count frequency of each string in stringList
+    for (const string& str : stringList) {
+        frequencyMap[str]++;
+    }
+    
+    // Query frequencies
+    vector<int> result;
+    result.reserve(queries.size());
+    for (const string& q : queries) {
+        result.push_back(frequencyMap[q]);
+    }
+    
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string stringList_count_temp;
+    getline(cin, stringList_count_temp);
+
+    int stringList_count = stoi(ltrim(rtrim(stringList_count_temp)));
+
+    vector<string> stringList(stringList_count);
+
+    for (int i = 0; i < stringList_count; i++) {
+        string stringList_item;
+        getline(cin, stringList_item);
+
+        stringList[i] = stringList_item;
+    }
+
+    string queries_count_temp;
+    getline(cin, queries_count_temp);
+
+    int queries_count = stoi(ltrim(rtrim(queries_count_temp)));
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(stringList, queries);
+
+    for (size_t i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !isspace(ch);
+        })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+            return !isspace(ch);
+        }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string token;
+    istringstream tokenStream(str);
+
+    while (getline(tokenStream, token, ' ')) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
